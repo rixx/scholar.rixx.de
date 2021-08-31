59931e0e20c17d43030dbb29260417b92321cb37
Tobias Kunze <r@rixx.de>
2021-08-31 22:58:17 +0200

    Initial data model
    … I wonder what painful mistakes it will turn out to contain

--- src/scholar/core/models.py
+++ src/scholar/core/models.py
@@ -1,3 +1,99 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.functional import cached_property
+from ordered_model.models import OrderedModel
+
+
+class BaseModel(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Topic(BaseModel):
+    """A Topic is a collection of cards, with a bit of metadata of its own.
+
+    Most of the metadata is calculated from cards – both its own and
+    related ones.
+    """
+
+    title = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True, db_index=True)
+
+    info_box = models.JSONField()
+    language = models.CharField(
+        choices=(("de", "German"), ("en", "English")), default="en", max_length=2
+    )
+    tags = models.ManyToManyField("Tag", related_name="topics")
+    translation = models.OneToOneField(
+        to="Topic", on_delete=models.SET_NULL, related_name="+", null=True
+    )
+
+    @cached_property
+    def sources(self):
+        return Source.objects.filter(cards__topic=self)
+
+
+class Card(OrderedModel, BaseModel):
+    """Cards don't have to have a topic: they can just belong to one source,
+    until a proper topic is found or made."""
+
+    text = models.TextField()
+    topic = models.ForeignKey(
+        to=Topic, on_delete=models.CASCADE, related_name="cards", null=True
+    )
+    sources = models.ManyToManyField("Source", through="CardSourceThrough")
+    references = models.ManyToManyField(Topic, through="CardTopicThrough")
+    prediction_deadline = models.DateTimeField(null=True)
+    prediction_result = models.BooleanField(null=True)
+
+    order_with_respect_to = "topic"
+
+    class Meta(OrderedModel.Meta):
+        pass
+
+    def generate_references(self):
+        pass  # TODO
+
+
+class Source(BaseModel):
+    title = models.CharField(max_length=100)
+    url = models.URLField(max_length=200, null=True)
+    trust = models.IntegerField(
+        choices=(
+            (0, "incorrect"),
+            (1, "probably incorrect"),
+            (2, "who knows, 50/50 of being correct (pop sci)"),
+            (3, "better than 50/50 at least (good pop sci)"),
+            (4, "good source"),
+            (5, "excellent source"),
+        )
+    )
+
+
+class CardSourceThrough(OrderedModel):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    order_with_respect_to = "card"
+
+    class Meta:
+        ordering = ("card", "source")
+
+
+class CardTopicThrough(OrderedModel):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    order_with_respect_to = "card"
+
+    class Meta:
+        ordering = ("card", "topic")
+
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=50)
+    slug = models.CharField(max_length=50, unique=True, db_index=True)
--- src/scholar/wsgi.py
+++ src/scholar/wsgi.py
@@ -1,5 +1,4 @@
-"""
-WSGI config for scholar project.
+"""WSGI config for scholar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
