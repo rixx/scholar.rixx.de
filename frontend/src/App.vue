6b10a545cc0fadfbf443940a895382486425217a
Tobias Kunze <r@rixx.de>
2021-09-02 01:29:58 +0200

    Display search results

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <header>
-    <img src="./assets/search.svg">
+    <div id="search-container" :class="{active: searching}">
+      <img src="./assets/search.svg" @click="toggleSearch()">
+      <input v-if="searching" v-model="searchTerm" @keydown.esc="toggleSearch()" @input="updateSearch()">
+      <div v-if="searching && searchResults.length" id="search-results">
+        <template v-for="result in searchResults" v-bind:key="result.id">
+          <a v-if="result.type === 'topic'" :href="'/t/' + result.title">
+            <strong>Topic: </strong> {{ result.title }}
+          </a>
+          <a v-else-if="result.type === 'source'" :href="'/source/' + result.id">
+            <strong>Source: </strong> {{ result.title }}
+          </a>
+          <a v-else-if="result.type === 'tag'" :href="'/tag/' + result.name">
+            <strong>Tag: </strong> {{ result.name }}
+          </a>
+        </template>
+      </div>
+    </div>
     <img src="./assets/add.svg">
     <img src="./assets/cards.svg">
     <img v-if="!user.token" @click="startLogin()" src="./assets/login.svg">
@@ -27,8 +43,11 @@ export default {
   data () {
     return {
       loggingIn: false,
+      searching: false,
       loginUser: "",
       loginPassword: "",
+      searchResults: [],
+      searchTerm: "",
     }
   },
   components: {
@@ -46,7 +65,24 @@ export default {
           store.setUser(this.loginUser, response.token)
         }
       })
-    }
+    },
+    toggleSearch () {
+      if (this.searching) {
+        this.searching = false
+      } else {
+        this.searching = true
+        window.setTimeout(() => document.querySelector("#search-container input").focus(), 5)
+      }
+    },
+    updateSearch () {
+      if (this.searchTerm.length == 0) return
+      const params = new URLSearchParams({q: this.searchTerm, short: true}).toString()
+      api.fetch(`/api/search/?${params}`, 'GET').then(response => {
+        if (response.result) {
+          this.searchResults = response.result
+        }
+      })
+    },
   },
   computed: {
     user () {
@@ -74,9 +110,50 @@ header {
   top: 64px;
   left: 20px;
 }
-header img {
+header > * {
   margin-bottom: 16px;
   cursor: pointer;
+}
+header img {
+  width: 40px;
+}
+header #search-container.active {
+  position: relative;
+  box-shadow: 0px 0px 20px 4px rgba(0,0,0,0.10);
+  border-radius: 44px;
+  padding: 4px;
+  display: flex;
+  width: 300px;
+}
+header #search-container.active img {
+  width: 28px;
+  margin-left: 12px;
+  margin-right: 8px;
+}
+header #search-container.active input {
+  border-radius: 44px;
+  margin: 0 4px;
+  width: 100%;
+  padding-left: 16px;
+}
+#search-results {
+  position: absolute;
+  top: 50px;
+  right: 24px;
+  width: 220px;
+  background: white;
+  display: flex;
+  flex-direction: column;
+}
+#search-results > a {
+  padding: 16px;
+  border: 1px solid #ddd;
+  border-top: none;
+  text-decoration: none;
+  color: #294370;
+}
+#search-results > a:hover {
+  background-color: #f4f4f4;
 }
 main {
   max-width: 800px;
@@ -121,10 +198,14 @@ main p {
   padding: 32px 64px;
   text-align: center;
 }
+input, input:focus {
+  font-size: 18px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  outline: none;
+}
 #login-overlay input {
   width: 50%;
-  font-size: 18px;
   margin: 15px auto;
-  padding: 8px;
 }
 </style>
